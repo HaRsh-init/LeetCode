954b5559b8eb7314a84ba058f90e8c357b397481
Harsh Raj <hrj9988@gmail.com>
2026-07-07 01:38:27 +0530

    Time: 3 ms (28%), Space: 9.9 MB (60.24%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    // Helper function with recursion
-    void generateSubsets(vector<int>& nums, int index, vector<int>& current, vector<vector<int>>& result) {
-        // BASE CASE: We've processed all elements
-        if (index == nums.size()) {
-            result.push_back(current);  // Add the current subset to result
-            return;
+    vector<vector<int>> subsets(vector<int>& nums) {
+
+        int n = nums.size();
+        int subset = 1<<n;
+
+        vector<vector<int>> ans;
+        for(int i=0; i<subset; i++){
+            vector<int> list;
+
+            for(int j=0; j<n; j++){
+                if(i&(1<<j)){
+                    list.push_back(nums[j]);
+                }
+            }
+            ans.push_back(list);
         }
         
-        // CHOICE 1: EXCLUDE nums[index]
-        generateSubsets(nums, index + 1, current, result);
-        
-        // CHOICE 2: INCLUDE nums[index]
-        current.push_back(nums[index]);  // Add it to current subset
-        generateSubsets(nums, index + 1, current, result);
-        current.pop_back();  // BACKTRACK: Remove it for other branches
-    }
-    
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> result;
-        vector<int> current;
-        generateSubsets(nums, 0, current, result);
-        return result;
+        return ans;
     }
 };
